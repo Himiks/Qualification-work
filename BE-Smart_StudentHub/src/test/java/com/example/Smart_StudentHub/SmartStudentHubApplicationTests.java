4452f49a91338d7d27f4ee3630451d723826f44b
Gleb Sorochenko <glebsorochenko@Mac.domain.lv>
2026-01-31 22:21:29 +0200

    application-test.yml added

--- BE-Smart_StudentHub/src/test/java/com/example/Smart_StudentHub/SmartStudentHubApplicationTests.java
+++ BE-Smart_StudentHub/src/test/java/com/example/Smart_StudentHub/SmartStudentHubApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.Smart_StudentHub;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SmartStudentHubApplicationTests {
 
 	@Test
