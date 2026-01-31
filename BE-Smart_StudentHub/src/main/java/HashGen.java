30b9ea48900ae836a6a20a1f7264f0de8a9caf3b
Gleb Sorochenko <glebsorochenko@Glebs-MacBook-Pro.local>
2026-01-31 20:30:55 +0200

    CI pipeline is implemented

--- BE-Smart_StudentHub/src/main/java/HashGen.java
+++ BE-Smart_StudentHub/src/main/java/HashGen.java
@@ -2,6 +2,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class HashGen {
     public static void main(String[] args) {
-        System.out.println(new BCryptPasswordEncoder().encode("newpassword123"));
+        System.out.println(new BCryptPasswordEncoder().encode("newpassword1223"));
     }
 }
